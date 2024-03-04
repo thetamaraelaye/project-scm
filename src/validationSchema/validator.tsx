5badadf61eca3609388551462da1a1e9dac7c129
thetamaraelaye <tamaraelaye@gmail.com>
2024-03-04 20:15:51 +0100

    feat(signup): start signup screen

--- src/validationSchema/validator.tsx
+++ src/validationSchema/validator.tsx
@@ -11,7 +11,7 @@ export const loginValidator = Yup.object({
 //   token: Yup.string().required("Token is required"),
 // });
 
-export const registerUserValidator = Yup.object({
+export const registerUserValidatorStepOne = Yup.object({
     first_name: Yup.string().required("Last Name is required"),
     last_name: Yup.string().required("First Name is required"),
     email: Yup.string()
@@ -19,22 +19,27 @@ export const registerUserValidator = Yup.object({
     .required("Email is required"),
     phone_number: Yup.string().required("Phone Number is required"),
     address: Yup.string(),
-    account_name: Yup.string(),
-    account_number:  Yup.string()
-    .min(10, 'Account number cannot be less than 10 digits')
-    .max(10, 'Exceeded characters for Account Number')
-    .required("Account Number is required").matches(
-      /^-?[0-9]+(.[0-9]{1-7})?$/,
-      "Enter a valid Account Number"),
-    bvn:  Yup.string()
-    .min(11, 'BVN number cannot be less than 11 digits')
-    .max(11, 'Exceeded characters for BVN Number')
-    .required("BVN is required").matches(
-      /^-?[0-9]+(.[0-9]{1-7})?$/,
-      "Enter a valid BVN Number"),
+    date_of_birth: Yup.date().required("Date of Birth is required"),
     password: Yup.string().required('Password is required'),
     confirm_password: Yup.string().required('Passwords must match').oneOf([Yup.ref('password')], 'Passwords must match'),
   });
+
+export const registerUserValidatorStepTwo = Yup.object({
+  account_name: Yup.string(),
+  account_number:  Yup.string()
+  .min(10, 'Account number cannot be less than 10 digits')
+  .max(10, 'Exceeded characters for Account Number')
+  .required("Account Number is required").matches(
+    /^-?[0-9]+(.[0-9]{1-7})?$/,
+    "Enter a valid Account Number"),
+  bank_name: Yup.string(),
+  bvn:  Yup.string()
+  .min(11, 'BVN number cannot be less than 11 digits')
+  .max(11, 'Exceeded characters for BVN Number')
+  .required("BVN is required").matches(
+    /^-?[0-9]+(.[0-9]{1-7})?$/,
+    "Enter a valid BVN Number"),
+})
 
 export const registerSupplierValidator = Yup.object({
   phone_number: Yup.string().required("Phone Number is required"),
