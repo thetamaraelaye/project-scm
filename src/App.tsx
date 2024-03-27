80b5187c577ff0717fdb443edef84ebb765748a1
Tamara <tamaraelaye.gmail.com>
2024-03-27 06:00:51 +0100

    add new path

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from './pages/auth/login';
-import './App.css';
-import Signup from './pages/auth/signup';
-import DashboardPage from './pages/admin/dashboard';
-import Sidebar from './component/Sidebar';
+import LoginPage from "./pages/auth/login";
+import "./App.css";
+import Signup from "./pages/auth/signup";
+import DashboardPage from "./pages/admin/dashboard";
+import Sidebar from "./component/Sidebar";
+import SetUpAccount from "./pages/auth/setUpAccount";
 // import AdminRoute from './component/PrivateRoutes/AdminRoutes';
 function App() {
   return (
@@ -25,15 +26,14 @@ function App() {
       </header> */}
       <Router>
         <Routes>
-         <Route path="/" element={<LoginPage />} />
-         <Route path="/sign-up" element={<Signup />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/setup-supplier-account" element={<SetUpAccount />} />
 
-         {/* <Route path="/" element={<DashboardPage/>} /> */}
-         <Route
+          {/* <Route path="/" element={<DashboardPage/>} /> */}
+          <Route
             path="admin/dashboard"
-            element={
-                <Sidebar title="Dashboard" children={<DashboardPage />} />
-            }
+            element={<Sidebar title="Dashboard" children={<DashboardPage />} />}
           />
         </Routes>
       </Router>
