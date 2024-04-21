88e97da030c0e80e466bd2c4b4c75e8e9e507e70
Tamara <tamaraelaye.gmail.com>
2024-04-21 15:36:51 +0100

    chore(helper): create outsideclick for modal and drawer

---
+++ src/helpers/outsideClick.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+
+export function useOutsideAlerter(
+  ref: React.RefObject<any>,
+  setOpen: (isOpen: boolean) => void
+) {
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setOpen(false);
+      }
+    }
+
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, setOpen]);
+}
